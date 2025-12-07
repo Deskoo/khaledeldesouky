649f646c596cb9677cadf0659257d322ea79fbab
khaledeldesouky <khaledmahmoudeldesoky671@gmail.com>
2025-12-07 18:18:05 +0200

    Add files via upload

---
+++ GUI code.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import webbrowser
+def fun():
+    link = txt.get()
+    webbrowser.open(link)
+
+
+myframe=Tk()
+myframe.geometry('400x300')
+myframe.title('My App')
+
+
+lbl=Label(myframe,text='Web Browser',fg='black',font=' Tahoma 30 bold')
+lbl.pack(pady=30)
+
+txt = Entry(myframe,width=50)
+txt.pack(pady=10)
+
+btn = Button(myframe,commond=fun,text='click on the link',fg='blue',bg='yellow',font='poplarStd 20 bold',padx=10)
+btn.pack()
+
+myframe.mainloop()
